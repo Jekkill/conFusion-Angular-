bde7b68a690b44bde9601a643757e9240fc51344
Pavel <pluhov-pavel@rambler.ru>
2017-08-09 13:59:20 +0300

    angular components part 1

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core'; /*it specifies that we import that module from angular core library */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'; /*enables to bootstrap your angular application by taking as a parametres */
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
