3e13c1fcec91cd9d92d2b217f3fb84b633e4fd6f
Pavel <pluhov-pavel@rambler.ru>
2017-08-16 13:08:04 +0300

    template forms part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { LeaderService } from './services/leader.service';
 
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -47,6 +49,9 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
   providers: [ DishService,
   PromotionService,
   LeaderService ],
+  entryComponents: [
+    LoginComponent
+  ],
   bootstrap: [AppComponent]
 })
 
