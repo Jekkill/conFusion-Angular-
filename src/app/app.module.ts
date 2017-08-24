6769e6c7d5d28bb7718f7be801258fb8d18a47c2
Pavel <pluhov-pavel@rambler.ru>
2017-08-24 12:53:31 +0300

    http client part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,9 @@ import { LeaderService } from './services/leader.service';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LoginComponent } from './login/login.component';
 
+import { baseURL } from './shared/baseurl';
+import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -50,7 +53,9 @@ import { LoginComponent } from './login/login.component';
   ],
   providers: [ DishService,
   PromotionService,
-  LeaderService ],
+  LeaderService,
+   { provide: 'BaseURL', useValue: baseURL },
+   ProcessHTTPMsgService ],
   entryComponents: [
     LoginComponent
   ],
